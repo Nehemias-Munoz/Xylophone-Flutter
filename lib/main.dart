5b30e9ef7e9a824df3b2c3e70f4c694242cb8bbd
Nehemias-Munoz <nehemiasmarvic.dev@gmail.com>
2021-08-25 11:51:52 -0400

    Refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -16,95 +16,32 @@ class XylophonePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+      padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.red)),
-            onPressed: () {
-              final player = AudioCache(prefix: 'assets/sounds/');
-              player.play('note1.wav');
-            },
-            child: SizedBox(
-              width: 400.0,
-              height: 70.0,
-            ),
-          ),
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.orange)),
-            onPressed: () {
-              final player = AudioCache(prefix: 'assets/sounds/');
-              player.play('note2.wav');
-            },
-            child: SizedBox(
-              width: 400.0,
-              height: 70.0,
-            ),
-          ),
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.yellow)),
-            onPressed: () {
-              final player = AudioCache(prefix: 'assets/sounds/');
-              player.play('note3.wav');
-            },
-            child: SizedBox(
-              width: 400.0,
-              height: 70.0,
-            ),
-          ),
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.green)),
-            onPressed: () {
-              final player = AudioCache(prefix: 'assets/sounds/');
-              player.play('note4.wav');
-            },
-            child: SizedBox(
-              width: 400.0,
-              height: 70.0,
-            ),
-          ),
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.teal)),
-            onPressed: () {
-              final player = AudioCache(prefix: 'assets/sounds/');
-              player.play('note5.wav');
-            },
-            child: SizedBox(
-              width: 400.0,
-              height: 70.0,
-            ),
-          ),
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.lightBlue)),
-            onPressed: () {
-              final player = AudioCache(prefix: 'assets/sounds/');
-              player.play('note6.wav');
-            },
-            child: SizedBox(
-              width: 400.0,
-              height: 70.0,
-            ),
-          ),
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.deepPurple)),
-            onPressed: () {
-              final player = AudioCache(prefix: 'assets/sounds/');
-              player.play('note7.wav');
-            },
-            child: SizedBox(
-              width: 400.0,
-              height: 70.0,
-            ),
-          ),
+          builKey(Colors.red, 1),
+          builKey(Colors.orange, 2),
+          builKey(Colors.yellow, 3),
+          builKey(Colors.green, 4),
+          builKey(Colors.teal, 5),
+          builKey(Colors.lightBlue, 6),
+          builKey(Colors.deepPurple, 7)
         ],
+      ),
+    );
+  }
+
+  Widget builKey(Color color, int number) {
+    return ElevatedButton(
+      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(color)),
+      onPressed: () {
+        final player = AudioCache(prefix: 'assets/sounds/');
+        player.play('note$number.wav');
+      },
+      child: SizedBox(
+        width: 400.0,
+        height: 85.0,
       ),
     );
   }
